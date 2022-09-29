7ce55a3446f59d5a8bb516c2c63f19462945ef15
kaspersoreide <kasper.soreide@gmail.com>
2022-09-29 14:13:01 +0200

    Unlink ARM build files and ignore

--- GNU ARM v10.3.1 - Debug/game.d
+++
@@ -1,5 +0,0 @@
-game.o: ../game.c \
- /home/kasper/SimplicityStudio/SDKs/gecko_sdk//platform/common/toolchain/inc/sl_gcc_preinclude.h \
- ../game.h
-/home/kasper/SimplicityStudio/SDKs/gecko_sdk//platform/common/toolchain/inc/sl_gcc_preinclude.h:
-../game.h:
