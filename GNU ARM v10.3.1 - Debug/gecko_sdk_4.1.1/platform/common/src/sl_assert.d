1a9167ce5bc058e57cdc9522a91b9980407f27e1
kaspersoreide <kasper.soreide@gmail.com>
2022-09-29 13:56:37 +0200

    untrack project files

--- GNU ARM v10.3.1 - Debug/gecko_sdk_4.1.1/platform/common/src/sl_assert.d
+++
@@ -1,6 +0,0 @@
-gecko_sdk_4.1.1/platform/common/src/sl_assert.o: \
- /home/kasper/SimplicityStudio/SDKs/gecko_sdk/platform/common/src/sl_assert.c \
- /home/kasper/SimplicityStudio/SDKs/gecko_sdk//platform/common/toolchain/inc/sl_gcc_preinclude.h \
- /home/kasper/SimplicityStudio/SDKs/gecko_sdk//platform/common/inc/sl_assert.h
-/home/kasper/SimplicityStudio/SDKs/gecko_sdk//platform/common/toolchain/inc/sl_gcc_preinclude.h:
-/home/kasper/SimplicityStudio/SDKs/gecko_sdk//platform/common/inc/sl_assert.h:
