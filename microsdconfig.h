cb0a33c0436510fe346377354cb884e661792d65
kaspersoreide <kasper.soreide@gmail.com>
2022-11-01 13:15:52 +0100

    needs testing

--- microsdconfig.h
+++ microsdconfig.h
@@ -16,19 +16,17 @@
 #ifndef __MICROSDCONFIG_H
 #define __MICROSDCONFIG_H
 
-/* TODO: set values correctly for our use case */
-
 /* Don't increase MICROSD_HI_SPI_FREQ beyond 24MHz. */
 #define MICROSD_HI_SPI_FREQ     24000000
 
 #define MICROSD_LO_SPI_FREQ     100000
-#define MICROSD_USART           USART0
+#define MICROSD_USART           USART2
 #define MICROSD_LOC             USART_ROUTE_LOCATION_LOC1
-#define MICROSD_CMUCLOCK        cmuClock_USART0
-#define MICROSD_GPIOPORT        gpioPortE
-#define MICROSD_MOSIPIN         7
-#define MICROSD_MISOPIN         6
-#define MICROSD_CSPIN           4
-#define MICROSD_CLKPIN          5
+#define MICROSD_CMUCLOCK        cmuClock_USART2
+#define MICROSD_GPIOPORT        gpioPortC
+#define MICROSD_MOSIPIN         3
+#define MICROSD_MISOPIN         2
+#define MICROSD_CSPIN           5
+#define MICROSD_CLKPIN          4
 
 #endif /* __MICROSDCONFIG_H */
