8ae50e2a4378375834eb142161e8353d4a386bff
kaspersoreide <kasper.soreide@gmail.com>
2022-11-01 14:51:14 +0100

    block modification works

--- game.h
+++ game.h
@@ -3,7 +3,7 @@
 
 #include "stdint.h"
 #include <stdbool.h>
-#define GAME_MAP_SIZE 64
+#define GAME_MAP_SIZE 16
 //TODO: find nice values for these
 #define TURN_SPEED 0.5
 #define MOVE_SPEED 0.8
@@ -63,10 +63,14 @@ void move_player(float move_x, float move_y, float dt);
 
 bool check_collision(float x_pos, float y_pos);
 
+void destroy_block();
+
+void place_block();
+
 //allocates memory and fills in initial state of map
 void init_map();
 
-void init_player(float x_pos, float y_pos);
+void init_player();
 
 extern GameBlock game_map[GAME_MAP_SIZE][GAME_MAP_SIZE];
 extern Player player;
